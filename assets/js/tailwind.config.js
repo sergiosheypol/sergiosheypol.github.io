241a6bbb77639fcd3b639bd568f696fa13f6137b
Sergio López <sergiolf96@gmail.com>
2022-06-18 10:46:33 +0200

    ++

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                c1: '#231942',
+                c2: '#5E548E',
+                c3: '#9F86C0',
+                c4: '#BE95C4',
+                c5: '#E0B1CB',
+            }
+        }
+    }
+}
